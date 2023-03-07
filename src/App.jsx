24becabdb4d99909b9e974be5de1c23fe3bd0736
AudunTA <aud-and2@online.no>
2023-03-07 20:16:42 +0100

    made a styled component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import "./App.css";
+import styled from "styled-components";
 
+const Card = styled.div`
+  border: 1px solid black;
+  background-color: white;
+  color: black;
+  border-radius: 8px;
+`;
 function App() {
   return (
     <div className="App">
-      <p>test</p>
+      <Card>
+        <p>Hello</p>
+      </Card>
     </div>
   );
 }
